fa133c2115c09b69d3c937b129391e6a042b7a92
Balazs Ando <elnesia@gmail.com>
2016-07-14 13:56:36 +0200

    Implement drawing

--- sample-image-generator.py
+++ sample-image-generator.py
@@ -1,19 +1,36 @@
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
+import random
+from text_box import TextBox
 
-img = Image.new("RGB", (512, 512), "red")
-draw = ImageDraw.Draw(img)
-# font = ImageFont.truetype(<font-file>, <font-size>)
-# font = ImageFont.truetype("sans-serif.ttf", 16)
-text_options = {
-    'fill': (255, 255, 255)
-}
-text_content = "Sample Text"
-text_size = draw.textsize(text_content)
-# draw.text((x, y),text_content,(r,g,b))
-draw.text((0, 0), text_content, **text_options)
-draw.text((0, text_size[1]), text_content, **text_options)
-draw.text((text_size[0], 0), text_content, **text_options)
-draw.text(text_size, text_content, **text_options)
+img = Image.new("RGB", (600, 600), "white")
+fonts = []
+fonts.append(ImageFont.truetype("Xposed.ttf", 10))
+fonts.append(ImageFont.truetype("Xposed.ttf", 16))
+fonts.append(ImageFont.truetype("Xposed.ttf", 22))
+fonts.append(ImageFont.truetype("Xposed.ttf", 26))
+fonts.append(ImageFont.truetype("Xposed.ttf", 48))
+
+
+sizes = [(60, 15), (120, 30), (180, 45), (240, 60), (300, 75)]
+available = [True]*400
+for i in TextBox.client():
+    text_box = Image.new("RGB", sizes[i.size-1], i.color)
+    draw = ImageDraw.Draw(text_box)
+    w, h = draw.textsize(i.text, fonts[i.size-1])
+    draw.text(((sizes[i.size-1][0]-w)/2, (sizes[i.size-1][1]-h)/2), i.text, (255, 255, 255), fonts[i.size-1])
+    free = False
+    while not free:
+        x = random.randint(0, 10-i.size)
+        y = random.randint(0, 40-i.size)
+        free = True
+        for j in range(i.size):
+            for k in range(i.size):
+                free = free and available[(y+j)*10+(x+k)]
+    for j in range(i.size):
+        for k in range(i.size):
+            available[(y+j)*10+(x+k)] = False
+    img.paste(text_box, (x*60, y*15))
 img.save('sample-out.png')
+img.show()
