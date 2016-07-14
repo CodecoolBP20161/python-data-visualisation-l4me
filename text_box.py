e5878c9beb0373895844aea7c5aac01aae43c7ad
Mate Gonczi <gamte19@gmail.com>
2016-07-14 11:19:42 +0200

    project method is done

--- text_box.py
+++ text_box.py
@@ -41,14 +41,51 @@ class TextBox():
                 size = 2
             else:
                 size = 1
-            color = avg_color
+            color = tuple(avg_color)
             text_boxes.append(TextBox(text, size, color))
             counter +=1
 
-
-        for i in text_boxes:
-            print(i.text, i.size, i. color)
+       # print(clients)
+        #for i in text_boxes:
+         #   print(i.text, i.size, i. color)
         return text_boxes
+
+    @staticmethod
+    def project():
+        data = Db.execute_query("SELECT name, budget_value, main_color, CASE WHEN budget_currency ='EUR' THEN '315' WHEN budget_currency = 'USD' THEN '260' WHEN budget_currency = 'GBP' THEN '400' ELSE budget_currency END FROM project")
+        project_lista = [i[0] for i in data if i[0] != None]
+        lista_color = [TextBox.normalize_color(i[2]) for i in data if i[0] != None]
+        currency = [(i[3]) for i in data if i[0] != None]
+        budget = [i[1] for i in data if i[0] != None]
+        currency = map(int, currency)
+        budget = map(float, budget)
+        exchanged_budget = [a*b for a,b in zip(budget, currency)]
+        ultimate = [[x, lista_color[i]] for i, x in enumerate(project_lista)]
+        a_ultimate = [[x[0],x[1], exchanged_budget[i]] for i,x in enumerate(ultimate)]
+        a_ultimate= list(sorted(a_ultimate, key = lambda x: x[2], reverse = True))
+
+
+        text_boxes = []
+        counter = 0
+        for i in a_ultimate:
+            text = i[0]
+            color = tuple(i[1])
+            if counter < 1:
+                size = 5
+            elif counter < 5:
+                size = 4
+            elif counter < 17:
+                size = 3
+            elif counter < 42:
+                size = 2
+            else:
+                size = 1
+            text_boxes.append(TextBox(text,size,color))
+            counter +=1
+
+        return text_boxes
+       # for i in text_boxes:
+        #    print(i.text, i.size, i.color)
 
 
     @staticmethod
@@ -63,4 +100,6 @@ class TextBox():
 
 
 
+
+TextBox.project()
 TextBox.client()
